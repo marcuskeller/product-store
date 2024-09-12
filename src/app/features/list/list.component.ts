378050451ca842ee834cbd6079087e442e33380b
Marcus <v.marcusk@gmail.com>
2024-09-12 15:50:08 -0300

    refactor: cria ConfirmationDialogService especializado na modal de confirmação

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,41 +1,13 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
-import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
-import { MatDialog, MatDialogModule, MatDialogRef } from "@angular/material/dialog";
 import { CardComponent } from './components/card/card.component';
 import { Router, RouterLink } from '@angular/router';
 import { filter } from 'rxjs';
+import { ConfimationDialogService, ConfirmationDialogComponent } from '../../shared/services/confimation-dialog.service';
 
-@Component({
-  selector: 'app-confirmation-dialog',
-  template: `
-  <h2 mat-dialog-title>Deletar produto</h2>
-  <mat-dialog-content>
-    Tem ceteza que quer deletar esse produto?
-  </mat-dialog-content>
-  <mat-dialog-actions align ="end">
-    <button mat-button (click)="onNo()">Não</button>
-    <button mat-raised-button color="accent" (click)="onYes()" cdkFocusInitial>Sim</button>
-  </mat-dialog-actions>
-`,
-  standalone: true,
-  imports: [MatButtonModule,MatDialogModule],
-})
-export class ConfirmationDialogComponent {
-  matDialogRef = inject(MatDialogRef);
 
-  onNo() {
-    this.matDialogRef.close(false);
-  }
-
-  onYes() {
-    this.matDialogRef.close(true);
-  }
-
-}
 
 @Component({
   selector: 'app-list',
@@ -49,7 +21,7 @@ export class ListComponent {
   
   productsService = inject(ProductsService);
   router = inject(Router);
-  matDialog = inject(MatDialog);
+  confimationDialogService = inject(ConfimationDialogService);
   
   ngOnInit() {
     this.productsService.getAll().subscribe((products) => {
@@ -62,15 +34,15 @@ export class ListComponent {
   }
   
   onDelete(product: Product) {
-    this.matDialog.open(ConfirmationDialogComponent)
-    .afterClosed()
+    this.confimationDialogService
+    .openDialog()
     .pipe(filter((answer) => answer === true))
     .subscribe(() => {
-      this.productsService.delete(product.id).subscribe(() => {
-        this.productsService.getAll().subscribe((products) => {
-          this.products = products;
+        this.productsService.delete(product.id).subscribe(() => {
+          this.productsService.getAll().subscribe((products) => {
+            this.products = products;
+          });
         });
-      });
     });
   }
 }
